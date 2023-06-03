6ee014a665842e030e8422f6b4744fda0c95a520
federico sivieri <sivieri.federico@gmail.com>
2023-06-03 15:06:56 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log('index')
+
+/**
+ * git init => inizializza la cartella di progetto con git
+ * git remote add origin <link_progetto> : collega il progetto locale al repository su github( o il server git)
+ */
