facf5e2387b945efa36eb1e2b4464ebc8a80e0cc
federico sivieri <sivieri.federico@gmail.com>
2023-06-03 15:13:00 +0200

    add doc for git

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ console.log('index')
 /**
  * git init => inizializza la cartella di progetto con git
  * git remote add origin <link_progetto> : collega il progetto locale al repository su github( o il server git)
+ * git add . : dice a git che tutti i file modificati andranno impacchettati nella commit
+ * git config user.email "you@example.com"
+ * git config user.name "your name"
+ * git commit -m "nome_commit": prepara la commit 
  */
